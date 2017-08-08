fbe8979c2d764d887c208a77f2ef942601a52eb7
Danielle Branch <dbranch82@gmail.com>
2017-08-08 09:45:31 -0600

    added image folder

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
+		<title>My Javascript playground</title>
 
 	</head>
-	<body></body>
+	<body>
+		<p>
+
+		</p>
+		<p>Mammasay mammasa mamma oo sa posuere shiznit bizzle. Phasellus a fo shizzle ut nibh away tincidunt. Maecenizzle mofo mammasay mammasa mamma oo sa. Doggy in shut the shizzle up sizzle i'm in the shizzle elementum tristique. Nunc break yo neck, yall tortizzle sizzle crunk sheezy crackalackin shizzlin dizzle. In velit tortor, ultricies izzle, bling bling quis, adipiscing quizzle, daahng dawg.
+			Etizzle gangster leo, aliquam consequat, pharetra nizzle, ghetto break it down, fo shizzle my nizzle. Tellivizzle bling bling. Cras lorem. Prizzle mammasay mammasa mamma oo sa erizzle izzle libero pot adipiscing. Fusce izzle hizzle crackalackin break it down break yo neck, yall mattizzle.
+			Phasellus dope sapizzle non erizzle. Suspendisse lorem leo, bling bling sed, mattizzle break yo neck, yall, commodo yo mamma, justo. Stuff fizzle ligula. Check it out feugiat, tellus a check out this tempizzle, get down get down metizzle phat ante, eget dapibizzle ma nizzle enim crazy lorizzle. Phasellus quam mah nizzle, imperdizzle shiznit, shiz crunk, sempizzle sheezy, uhuh ... yih!.
+			Bow wow wow izzle magna vel ipsizzle. Sizzle shiznit nibh, pimpin' vitae, vestibulum pizzle, break it down eu, velit. Yo </p>
+	</body>
 </html>
